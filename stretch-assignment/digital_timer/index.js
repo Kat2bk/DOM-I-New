d2d3c556c78fa995414c831d610051a6cb34d8d5
unknown <Kat2bk@gmail.com>
2020-04-01 11:39:01 -0700

    set up three functions within the Timer Constructor function...one that runs the time, one that passes date.now, another that formats the time

--- stretch-assignment/digital_timer/index.js
+++ stretch-assignment/digital_timer/index.js
@@ -1,18 +1,49 @@
 function Timer() {
-    var time = 0;
+  var time = 0;
+  var interval;
+  var offset;
+
+  function running() {
+    time += delta();
+    var formatTime = timeform(time);
+  }
+  function delta() {
+    var now = Date.now();
+    var timePassed = now - offset;
+    offset = now;
+    return timePassed;
+  }
+  function timeform(milliseconds) {
+    var time = new Date(milliseconds);
+  }
+
   this.isOn = false;
   this.start = function() {
-    console.log("timer is starting");
+    if (!this.isOn) {
+      interval = setInterval(running, 10);
+      offset = Date.now();
+      //   time when the stopwatch starts
+      this.isOn = true;
+      // turns it on
+    }
   };
-  this.stop;
-  this.reset;
-  init : function () {
-      time = document.querySelectorAll(".digit")
-  }
+  this.stop = function() {
+    if (this.isOn) {
+      clearInterval(interval);
+      interval = null;
+      this.isOn = false;
+    }
+  };
+  this.reset = function() {
+    time = 0;
+  };
+  //   init : function() {
+  //       time = document.querySelectorAll(".digit")
+  //   }
 }
 
 var watch = new Timer();
 
-function stopTimer() {
-  clearInterval(watch);
-}
+// function stopTimer() {
+//   clearInterval(watch);
+// }
